771e3476bdf0e5d78e10ee65595afa15137405c5
Chris Gantes <78065304+ChristopherGantes@users.noreply.github.com>
2025-01-21 23:30:47 -0500

    finished for now. have all the functionality required

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ function Book(title, author, numberOfPages, read) {
   this.numberOfPages = numberOfPages;
   this.read = read;
 
-  function info() {
+  this.info = function () {
     return `${title} by ${author}, ${numberOfPages} pages, ${
       read ? "read" : "not read yet"
     }`;
-  }
+  };
 
-  function toggleRead() {
+  this.toggleRead = function () {
     this.read = !this.read;
-  }
+  };
 }
 
 function addBookToLibrary(title, author, numberOfPages, read, library) {
@@ -24,37 +24,94 @@ function addBookToLibrary(title, author, numberOfPages, read, library) {
 
 const catcher = new Book("Catcher in The Rye", "J.D. Salinger", 200, true);
 const enders = new Book("Ender's Game", "Orson Scott Card", 300, false);
+const dune = new Book("Dune", "Frank Herbert", 340, false);
+const hitchhiker = new Book(
+  "The Hitchhiker's Guide to The Galaxy",
+  "Douglas Adams",
+  160,
+  true
+);
 
-myLibrary.push(catcher, enders);
-
+myLibrary.push(catcher, enders, dune, hitchhiker);
 
 const libraryGrid = document.querySelector("#library-grid");
 
-for (const book of myLibrary) {
+function createBookNode(book) {
   const bookDiv = document.createElement("div");
-  bookDiv.setAttribute("class", "book")
+  bookDiv.setAttribute("class", "book");
 
   const bookTitle = document.createElement("p");
   bookTitle.textContent = book.title;
-  bookTitle.setAttribute("class", "book-title")
-  
+  bookTitle.setAttribute("class", "book-title");
+
   const bookAuthor = document.createElement("p");
   bookAuthor.textContent = book.author;
-  bookAuthor.setAttribute("class", "book-author")
+  bookAuthor.setAttribute("class", "book-author");
 
   const bookPages = document.createElement("p");
-  bookPages.textContent = book.numberOfPages;
-  bookPages.setAttribute("class", "book-pages")
+  bookPages.textContent = `${book.numberOfPages} pages`;
+  bookPages.setAttribute("class", "book-pages");
 
-  const bookRead = document.createElement("p");
-  bookRead.textContent = book.read?"Read":"Unread";
-  bookRead.setAttribute("class", "book-read")
+  const bookReadButton = document.createElement("button");
+  bookReadButton.setAttribute("class", "book-read");
+  bookReadButton.textContent = book.read ? "Read" : "Unread";
+  bookReadButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    book.toggleRead();
+    bookReadButton.textContent = book.read ? "Read" : "Unread";
+  });
 
+  const removeButton = document.createElement("button");
+  removeButton.setAttribute("id", "remove-book");
+  removeButton.textContent = "X";
+  removeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const indexOfBook = myLibrary.indexOf(book);
+    myLibrary.splice(indexOfBook, 1);
+    bookDiv.parentNode.removeChild(bookDiv);
+    console.log(myLibrary);
+  });
 
   bookDiv.appendChild(bookTitle);
   bookDiv.appendChild(bookAuthor);
   bookDiv.appendChild(bookPages);
-  bookDiv.appendChild(bookRead);
+  bookDiv.appendChild(bookReadButton);
+  bookDiv.appendChild(removeButton);
 
-  libraryGrid.appendChild(bookDiv);
+  return bookDiv;
 }
+
+function buildLibrary(library) {
+  libraryGrid.replaceChildren();
+  library.forEach((book, index) => {
+    const bookNode = createBookNode(book);
+    bookNode.setAttribute("data", index);
+    libraryGrid.appendChild(bookNode);
+  });
+}
+
+buildLibrary(myLibrary);
+
+const showDialogButton = document.querySelector("#show-dialog");
+const bookDialog = document.querySelector("dialog");
+showDialogButton.addEventListener("click", () => {
+  bookDialog.showModal();
+});
+
+const bookForm = document.querySelector("#book-form");
+bookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = new FormData(bookForm);
+
+  addBookToLibrary(
+    data.get("title"),
+    data.get("author"),
+    Number(data.get("pages")),
+    data.get("read") === "on" ? true : false,
+    myLibrary
+  );
+  buildLibrary(myLibrary);
+
+  bookForm.reset();
+  bookDialog.close();
+});
